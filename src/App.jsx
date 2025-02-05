8a053d717c3be15c908ef412cf4677f474441a82
brettl <lukas.brettl@seznam.cz>
2025-02-05 21:17:10 +0100

    add basic structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@
 export default function App(){
     return(
         <>
-            <h1>Hello</h1>
+            <nav>
+
+            </nav>
+            <section>
+                <div className="relative">
+                    <img src="" alt="" />
+                    <span className="absolute bottom-0">stars</span>
+                </div>
+                
+                <div>
+                    <div className="flex">
+                        <h1>Name</h1>
+                        <h2>(year)</h2>
+                    </div>
+                    <div className="flex">
+                        <div>types of movie</div>
+                        <h3>time(h,m)</h3>
+                    </div>
+                    <div className="flex">
+                        <p>information about movie</p>
+                        <span>percent%</span>
+                    </div>
+                    <div>
+                        <button>ignore</button>
+                        <button>Add to play later list</button>
+                    </div>
+                </div>
+            </section>
+            <footer>
+
+            </footer>
         </>
     )
 }
