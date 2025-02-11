edf6f6e7fd1226f8fd7a68271294ee599b05f777
brettl <lukas.brettl@seznam.cz>
2025-02-11 21:12:13 +0100

    add image (if image of movie loading, this image shows up)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-
+import movie from "./assets/images/movie.jpg"
 export default function App(){
     return(
         <>
             <nav>
-
+                
             </nav>
-            <section>
+            <section className="flex w-full h-full items-center px-6 text-white">
                 <div className="relative">
-                    <img src="" alt="" />
+                    <img src={movie} alt="" className="w-80"/>
                     <span className="absolute bottom-0">stars</span>
                 </div>
                 
                 <div>
-                    <div className="flex">
-                        <h1>Name</h1>
-                        <h2>(year)</h2>
+                    <div className="flex items-end">
+                        <h1 className="text-5xl font-medium">Name</h1>
+                        <h2 className="text-2xl ml-2">(year)</h2>
                     </div>
-                    <div className="flex">
-                        <div>types of movie</div>
-                        <h3>time(h,m)</h3>
+                    <div className="flex items-center mt-3">
+                        <div className="p-1 px-3 font-semibold bg-red-900 rounded-full">types of movie</div>
+                        <h3 className="text-gray-400 ml-3">time(h,m)</h3>
                     </div>
                     <div className="flex">
                         <p>information about movie</p>
