62aa26a4307b33266d483a0b41a52600665cdcd8
brettl <lukas.brettl@seznam.cz>
2025-02-14 19:50:56 +0100

    add basic functions in Informations.jsx

--- src/Information.jsx
+++ src/Information.jsx
@@ -1,14 +1,32 @@
-
+import { useState } from "react"
+import App from "./App.jsx"
 export default function Information(){
-    const questions = {
-        "Name":"write your name/nick",
-        "Genre": "Select yours genres (min.3)",
-        "Keywords": "Write keywords that describe what you want to see (min.3)"
-    }
-    return(
-        <>
-            <input type="text" />
+    const [questionNumber, setQuestionNumber] = useState(0)
+    const [app, setApp] = useState(false)
 
-        </>
+    const QuestionsArray = ["Name", "Genre", "Keywords"]
+    const questions = {
+        Name:"write your name/nick",
+        Genre: "Select yours genres (min.3)",
+        Keywords: "Write keywords that describe what you want to see (min.3)"
+    }
+    
+    if(app) return <App/>
+    return(
+        <div className="text-white">
+            {localStorage.getItem("Name") && localStorage.getItem("Genre") && localStorage.getItem("Keywords")? <App/>
+            : 
+            <>
+                <h2>{QuestionsArray[questionNumber]}</h2>
+                
+                <input type="text" placeholder={questions[questionNumber[questionNumber]]}/>
+                {questionNumber < 2 ? <button onClick={() => setQuestionNumber(questionNumber + 1)}>next</button>
+                :<button onClick={() => setApp(true)}>finish</button>}
+            </>
+            
+            
+            }
+
+        </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Information from './Information.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Information />
   </StrictMode>,
 )
