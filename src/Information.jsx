9aa7585d71d9a53b9f8117aa5d4534bd47befaef
brettl <lukas.brettl@seznam.cz>
2025-02-17 21:22:55 +0100

    min 3 values on keywords and genre

--- src/Information.jsx
+++ src/Information.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx"
 export default function Information(){
     const [questionNumber, setQuestionNumber] = useState(0)
     const [app, setApp] = useState(false)
+    const [storageArray, setStorageArray] = useState([])
 
     const inputRef = useRef(null)
 
@@ -16,22 +17,49 @@ export default function Information(){
     if(app) return <App/>
     return(
         <div className="text-white">
-            {localStorage.getItem("Name") && localStorage.getItem("Genre") && localStorage.getItem("Keywords")? <App/>
+            {localStorage.getItem("Name") && (JSON.parse(localStorage.getItem("Genre") || "[]")).length >2 && (JSON.parse(localStorage.getItem("Keywords") || "[]")).length >2 ? <App/>
             : 
             <>
                 <h2>{QuestionsArray[questionNumber]}</h2>
                 
-                <input ref={inputRef} type="text" placeholder={questions[questionNumber[questionNumber]]}/>
-                {questionNumber < 2 ? <button onClick={() => {
-                    setQuestionNumber(questionNumber + 1)
-                    localStorage.setItem(QuestionsArray[questionNumber], inputRef.current.value)
+                <input ref={inputRef} type="text" placeholder={questions[QuestionsArray[questionNumber]]}/>
+
+                {["Genre", "Keywords"].includes(QuestionsArray[questionNumber]) && <button className="bg-amber-500" onClick={() =>{
+                    setStorageArray([...storageArray,inputRef.current.value])
                     inputRef.current.value = ""
+                    
+                }}>add</button>}
+
+                {questionNumber < 2 ? <button onClick={() => {
+
+                    if(questionNumber == 0){
+                        localStorage.setItem(QuestionsArray[questionNumber], JSON.stringify(inputRef.current.value))
+                    }
+
+                    else{
+                        if(storageArray.length >= 3){
+                            localStorage.setItem(QuestionsArray[questionNumber], JSON.stringify(storageArray))
+                            setStorageArray([])
+                        }
+                        else{
+                           return window.alert("minimum 3 values")
+                        }
+                    }
+
+                    inputRef.current.value = ""
+                    setQuestionNumber(questionNumber + 1)
                 }}>next</button>
                 
                 :<button onClick={() => {
-                    setApp(true)
-                    localStorage.setItem(QuestionsArray[questionNumber], inputRef.current.value)
+                    if(storageArray.length >= 3){
+                        localStorage.setItem(QuestionsArray[questionNumber], JSON.stringify(storageArray))
+                        setStorageArray([])
+                    }
+                    else{
+                       return window.alert("minimum 3 values")
+                    }
                     inputRef.current.value = ""
+                    setApp(true)
                 }}>finish</button>}
             </>
             
